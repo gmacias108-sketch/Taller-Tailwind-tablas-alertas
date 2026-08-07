fe5f8966e7b9d66249a7e40ba63b2c337768e2c0
jhonfredyr <riveracjhonf@gmail.com>
2026-08-07 15:57:47 -0500

    avances jhon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Formulario } from './formulario/formulario';
+import { Listar } from './listar/listar';
+import { Actualizar } from './actualizar/actualizar';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'productos',
+    component: Listar
+  },
+
+  {
+    path: 'productos/crear',
+    component: Formulario
+  },
+
+  {
+    path: 'productos/actualizar',
+    component: Actualizar
+  }
+
+];
