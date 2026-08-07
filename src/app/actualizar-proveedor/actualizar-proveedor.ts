b621ab85dc49d85549d941d819011217c409a9da
Nicole Valentina García Mosquera <nicole.gm199@gmail.com>
2026-08-06 21:05:17 -0500

    Se crea 3 componentes de tabla proveedores, actualizar-proveedor, formulario-proveedor, listar-proveedor

---
+++ src/app/actualizar-proveedor/actualizar-proveedor.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actualizar-proveedor',
+  imports: [],
+  templateUrl: './actualizar-proveedor.html',
+})
+export class ActualizarProveedor {
+
+}
---
+++ src/app/listar-proveedor/listar-proveedor.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-proveedor',
+  imports: [],
+  templateUrl: './listar-proveedor.html',
+})
+export class ListarProveedor {
+
+}
