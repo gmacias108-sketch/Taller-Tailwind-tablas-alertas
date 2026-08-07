3ecfeaf326d916797aaca13e4cb2ce2dd6495d2d
Gilbert Macias <gmacias108@gmail.com>
2026-08-07 18:02:40 -0500

    Se agregan formulariofactura, listarfactura y actualizarfactura; se realizan cambios en app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 import { Formulario } from './formulario/formulario';
 import { Listar } from './listar/listar';
 import { Actualizar } from './actualizar/actualizar';
+import { Formulariofactura } from './Factura/formulariofactura/formulariofactura';
+import { Listarfactura } from './Factura/listarfactura/listarfactura';
+import { Actualizarfactura } from './Factura/actualizarfactura/actualizarfactura';
+
 
 export const routes: Routes = [
 
@@ -18,6 +22,23 @@ export const routes: Routes = [
   {
     path: 'productos/actualizar',
     component: Actualizar
+  },
+
+  {
+    path: '',
+    component: Formulariofactura
+  },
+
+  {
+    path: 'listarfactura',
+    component: Listarfactura
+  },
+
+  {
+    path: 'actualizarfactura/:id',
+    component: Actualizarfactura
   }
+
+  
 
 ];
