15f171ed1268e90ffa89731038b605011431fca8
Jader Ortega <ortegarevelojader99@gmail.com>
2026-08-09 22:13:49 -0500

    alertas y estilos

--- src/app/Factura/actualizarfactura/actualizarfactura.ts
+++ src/app/Factura/actualizarfactura/actualizarfactura.ts
@@ -1,51 +1,50 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
-selector: 'app-actualizarfactura',
-imports: [FormsModule],
-templateUrl: './actualizarfactura.html',
+  selector: 'app-actualizarfactura',
+  standalone: true,
+  imports: [FormsModule, CommonModule, HttpClientModule],
+  templateUrl: './actualizarfactura.html',
 })
 export class Actualizarfactura {
+  id: number = 0;
+  factura: any = {};
 
-id: number = 0;
+  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) {}
 
-factura = {
-no_factura: '',
-fecha: '',
-cod_cliente: '',
-total_factura: ''
-};
+  ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+    this.cargarFactura();
+  }
 
-constructor(
-private http: HttpClient,
-private router: Router,
-private route: ActivatedRoute
-) {}
+  cargarFactura() {
+    this.http.get<any[]>(`https://srrpeanqjqfxtnuwhjez.supabase.co/rest/v1/factura?id=eq.${this.id}`,
+      { headers: { apikey: "sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x", Authorization: "Bearer sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x" } }
+    ).subscribe({
+      next: (res) => {
+        if(res.length > 0) {
+          this.factura = res[0];
+          this.factura.fecha = this.factura.fecha.split('T')[0]; // Arreglo para el input date
+        } else {
+          Swal.fire('Error', 'Factura no encontrada', 'error');
+          this.router.navigate(['/listarfactura']);
+        }
+      },
+      error: () => Swal.fire('Error', 'No se pudo cargar la factura', 'error')
+    });
+  }
 
-ngOnInit() {
-this.id = this.route.snapshot.params['id'];
-}
-
-actualizarFactura() {
-this.http.patch(
-"https://srrpeanqjqfxtnuwhjez.supabase.co/rest/v1/factura?id=eq." + this.id,
-this.factura,
-{
-headers: {
-apikey: "sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x",
-Authorization: "Bearer sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x",
-'Content-Type': 'application/json'
-}
-}
-)
-.subscribe({
-next: (respuesta) => {
-alert("Factura actualizada correctamente");
-this.router.navigate(['/listarfactura']);
-}
-});
-}
+  actualizarFactura() {
+    this.http.patch(`https://srrpeanqjqfxtnuwhjez.supabase.co/rest/v1/factura?id=eq.${this.id}`, this.factura,
+      { headers: { apikey: "sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x", Authorization: "Bearer sb_publishable_qnp1xzi89N_0c2Yex-wbwQ_ddmCG28x", 'Content-Type': 'application/json', 'Prefer': 'return=representation' } }
+    ).subscribe({
+      next: () => { Swal.fire('¡Actualizado!', 'Factura actualizada correctamente', 'success'); this.router.navigate(['/listarfactura']); },
+      error: () => Swal.fire('Error', 'No se pudo actualizar la factura', 'error')
+    });
+  }
 }
